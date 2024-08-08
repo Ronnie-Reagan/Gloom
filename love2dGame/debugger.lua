ed0383dc663d48d75e4509b604b4fc65f94ea6c5
Don Reagan <147023412+Ronnie-Reagan@users.noreply.github.com>
2024-08-08 15:05:06 -0700

    Update debugger.lua
    converted to a global system

--- love2dGame/debugger.lua
+++ love2dGame/debugger.lua
@@ -1,34 +1,39 @@
 -- debug.lua
-local debug = {}
-local maxdebugMessages = 200
-local debugMessages = {}
+_Gdebug = {}
+_GmaxdebugMessages = 2000
+_GdebugMessages = {}
 
+-- Log a message to the debug console and print it to the standard output
 function debug.log(message)
-    if #debugMessages >= maxdebugMessages then
-        table.remove(debugMessages, 1)
+    if #_GdebugMessages >= _GmaxdebugMessages then
+        _GdebugMessages = {}
     end
-    table.insert(debugMessages, message)
+    table.insert(_GdebugMessages, message)
     print(message)
 end
 
+-- Clear all debug messages
 function debug.clear()
-    debugMessages = {}
+    _GdebugMessages = {}
 end
 
+-- Save all debug messages to a file
 function debug.save()
-    local file = love.filesystem.newFile(os.date("%Y-%m-%d_%H-%M-%S") .. "_debug.log", "w")
-    for _, message in ipairs(debugMessages) do
+    local fileName = os.date("%Y-%m-%d_%H-%M-%S") .. "_debug.log"
+    local file = love.filesystem.newFile(fileName, "w")
+    for _, message in ipairs(_GdebugMessages) do
         file:write(message .. "\n")
     end
     file:close()
-    debug.log("debug messages saved to debug_log.txt")
+    debug.log("Debug messages saved to " .. fileName)
 end
 
+-- Draw debug messages on the screen if debug mode is enabled
 function debug.draw()
     if CONFIG and CONFIG.GAME and CONFIG.GAME.debug then
         love.graphics.setColor(1, 1, 1)
         love.graphics.setFont(love.graphics.newFont(12))
-        for i, message in ipairs(debugMessages) do
+        for i, message in ipairs(_GdebugMessages) do
             love.graphics.print(message, 10, 10 + (i - 1) * 15)
         end
     end
